58061108a7e7aee47af1a86dd421689840255441
tchang135 <tchang135@gmail.com>
2022-09-19 16:18:13 -0400

    Wrote methods until #enroll

--- bootcamp_project/lib/bootcamp.rb
+++ bootcamp_project/lib/bootcamp.rb
@@ -1,3 +1,36 @@
 class Bootcamp
-  
+    initialize(name, slogan, student_capacity)
+        @name = name
+        @slogan = slogan 
+        @student_capacity = student_capacity
+        @teachers = []
+        @students = []
+        @grades = Hash.new { |k,v| k[v] = []}
+    end
+
+    def name 
+        @name
+    end 
+
+    def slogan 
+        @slogan 
+    end 
+
+    def teachers 
+        @teachers 
+    end 
+
+    def students 
+        @students 
+    end 
+
+    def hire(string)
+        @teachers << string
+    end 
+
+    def enroll(string)
+        @students << string
+    end 
+    
+
 end
